a24208c8630493848d05baf254cc5e1d6be88e82
VICENTE MAURICIO QUINSAMOLLE VARBAS <qmaury30@gmail.com>
2025-03-13 17:34:49 -0400

    agregado nodemon para facilitar el desarollo

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('<html> <tittle>PAGINA 1</tittle> <h1 class="Loquesea">hola</h1></html>')
+})
+
+
+
+app.listen(port, () => {
+    console.log(`puerto iniciado en:  ${port}`)
+    //puerto 3k 
+})
