a9e8bdd3c09772da74e2bf4ee0b2a31198dc6313
VICENTE MAURICIO QUINSAMOLLE VARBAS <qmaury30@gmail.com>
2025-03-13 18:14:59 -0400

    conexion con supabase

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const app = express()
+const userRoutes = require('./routes/userRoutes');
+const errorHandler = require('./middleware/errorHandler');
+
 const port = 4000
+
+app.use(express.json());
+
+app.use('/api', userRoutes);
+
+
+app.use(errorHandler);
+
 
 app.get('/', (req, res) => {
     res.send('<html> <tittle>PAGINA 1</tittle> <h1 class="Loquesea">hola</h1></html>')
