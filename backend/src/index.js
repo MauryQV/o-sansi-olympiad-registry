36071d3699fd735b29c17b60907132606beaecc9
VICENTE MAURICIO QUINSAMOLLE VARBAS <qmaury30@gmail.com>
2025-03-14 09:11:40 -0400

    configuraciones finales en index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,22 +4,22 @@ const userRoutes = require('./routes/userRoutes');
 const competidorRoutes = require('./routes/competidorRoutes');
 const errorHandler = require('./middleware/errorHandler');
 const port = process.env.PORT;
-
 app.use(express.json());
+app.use(errorHandler);
 
 app.use('/api', userRoutes);
 app.use('/api', competidorRoutes);
-
-app.use(errorHandler);
 
 
 app.get('/', (req, res) => {
     res.send('<html> <title>PAGINA 1</title> <h1 class="Loquesea">hola</h1></html>')
 })
 
+app.get('/about', (req, res) => {
+    res.send('<title>About</title> <h1> SOBRE NOSOTROS </h1>')
+})
 
 
 app.listen(port, () => {
     console.log(`puerto iniciado en:  ${port}`)
-    //puerto 3k 
 })
