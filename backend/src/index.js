7e95680d928565c3c13eaf580d602ea9d338c48e
VICENTE MAURICIO QUINSAMOLLE VARBAS <qmaury30@gmail.com>
2025-03-18 10:24:04 -0400

    estructura del proyecto modificada a modular

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const userRoutes = require('./routes/userRoutes');
-const competidorRoutes = require('./routes/competidorRoutes');
+const fileRoutes = require('./routes/fileRoutes');
 const errorHandler = require('./middleware/errorHandler');
 const cors = require('cors');
 const port = process.env.PORT;
@@ -14,16 +14,14 @@ app.use(express.json());
 app.use(errorHandler);
 
 app.use('/api', userRoutes);
-app.use('/api', competidorRoutes);
+app.use('/api', fileRoutes);
 
 
 app.get('/', (req, res) => {
     res.send('<html> <title>PAGINA 1</title> <h1 class="Loquesea">hola</h1></html>')
 })
 
-app.get('/about', (req, res) => {
-    res.send('<title>About</title> <h1> SOBRE NOSOTROS </h1>')
-})
+
 
 
 app.listen(port, () => {
