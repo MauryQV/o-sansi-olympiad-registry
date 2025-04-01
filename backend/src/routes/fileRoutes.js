a8dedb2bdd79af0a0ad5296dc107eee4ef3422b4
VICENTE MAURICIO QUINSAMOLLE VARBAS <qmaury30@gmail.com>
2025-04-01 17:09:58 -0400

    reinicio del proyecto

--- backend/src/routes/fileRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const upload = require('../middleware/uploadMiddleware');
-const { subirExcel } = require('../controllers/fileController');
-
-
-router.post('/excel', upload.single('archivo'), subirExcel);
-
-module.exports = router;
