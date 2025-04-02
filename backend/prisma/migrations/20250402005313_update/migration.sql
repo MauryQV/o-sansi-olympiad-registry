39b5860d04b164641ad292ce248c86693f341992
VICENTE MAURICIO QUINSAMOLLE VARBAS <qmaury30@gmail.com>
2025-04-01 21:26:26 -0400

    orm test

---
+++ backend/prisma/migrations/20250402005313_update/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Competidor" (
+    "id" TEXT NOT NULL,
+    "nombre" TEXT NOT NULL,
+    "apellido" TEXT NOT NULL,
+    "carnet" TEXT NOT NULL,
+
+    CONSTRAINT "Competidor_pkey" PRIMARY KEY ("id")
+);
