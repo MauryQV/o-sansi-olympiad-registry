93fcb661e1c5184a348fc71956a315f72ac152de
Jhazmin <portuguez.colque.jhazmin25@gmail.com>
2025-05-01 22:28:36 -0400

    Actualización en mi rama

--- frontend/src/api/axiosInstance.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-    baseURL: 'http://localhost:7777/api', // ajustá si es necesario
-});
-
-export default instance;
--- frontend/src/components/Convocatorias/EstadoBadge.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import '../../styles/Convocatorias/EstadoBadge.css';
-
-const EstadoBadge = ({ estado }) => {
-  const clase = estado.toLowerCase().replace(/\s/g, '-');
-  return <span className={`estado-badge ${clase}`}>{estado}</span>;
-};
-
-export default EstadoBadge;
